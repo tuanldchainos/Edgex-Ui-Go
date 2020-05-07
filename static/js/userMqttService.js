7228390ebd41b1a951fb4da9407d98282f4d2043
tuanldchainos <tuanldhaihaua@gmail.com>
2020-05-07 22:34:42 +0700

    update putConfig for Mqtt service

--- static/js/userMqttService.js
+++ static/js/userMqttService.js
@@ -5,7 +5,7 @@ orgEdgexFoundry.userMqtt = (function(){
     "use strict";
 
     function UserMqtt(){
-        this.Mqtt = 'edgex-core-data'
+        this.Mqtt = 'mqtt-app'
     }
 
     UserMqtt.prototype = {
@@ -16,25 +16,72 @@ orgEdgexFoundry.userMqtt = (function(){
 
     var userMqttService = new UserMqtt()
 
-    // UserMqtt.prototype.connectMqtt = function() {
-    //     var host = document.getElementById
+    UserMqtt.prototype.connectMqtt = function() {
+        var username = document.getElementById("client-name").value
+        var password = document.getElementById("client-pass").value
+        var clientId = document.getElementById("client-pass").value
+        var qos = document.getElementById("connect-qos").value
+        var autoReconnect = document.getElementById("connect-arc").value
+        var retained = document.getElementById("connect-retained").value
+        var keepAlive = document.getElementById("connect-kal").value
+        var connectTimeout = document.getElementById("connect-ct").value
+        var skipCert = document.getElementById("sercure-scv").value
+        var key = document.getElementById("sercure-key").value
+        var cert = document.getElementById("sercure-cert").value
+        var pubHost = document.getElementById("publish-host").value
+        var pubPort = document.getElementById("publish-port").value
+        var pubPro = document.getElementById("publish-protocol").value
+        var subHost = document.getElementById("subscribe-host").value
+        var subPort = document.getElementById("subscribe-port").value
+        var subPro = document.getElementById("subscribe-protocol").value
+        var request = document.getElementById("topic-request").value
+        var response = document.getElementById("topic-response").value
 
-    //     var dataMqtt = {
-
-    //     }
-    //     $.ajax({
-    //         url: '/api/v1/user/config/appservice/' + userMqttService.Mqtt,
-    //         type: 'POST',
-    //         contentType: 'application/json',
-    //         data: ,
-    //         success: function(data) {
-    //             alert(data)
-    //         },
-    //         error: function() {
-    //             alert("faile to update service config, please try again")
-    //         }
-    //     })
-    // }
+        var dataMqtt = {
+            "Binding": {
+                "SubscribeTopic": request,
+                "PublishTopic": response
+            },
+            "MessageBus": {
+                "SubscribeHost": {
+                    "Host": pubHost,
+                    "Port": pubPort,
+                    "Protocol": pubPro
+                },
+                "PublishHost": {
+                    "Host": subHost,
+                    "Port": subPort,
+                    "Protocol": subPro
+                },
+                "Optional": {
+                    "Username": username,
+                    "Password": password,
+                    "ClientId": clientId,
+                    "Qos": qos,
+                    "KeepAlive": keepAlive,
+                    "Retained": retained,
+                    "AutoReconnect": autoReconnect,
+                    "ConnectTimeout": connectTimeout,
+                    "SkipCertVerify": skipCert,
+                    "KeyPEMBlock": key,
+                    "CertPEMBlock": cert
+                }
+            }
+        }
+        console.log(dataMqtt)
+        $.ajax({
+            url: '/api/v1/user/config/appservice/' + userMqttService.Mqtt,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(dataMqtt),
+            success: function(data) {
+                alert(data)
+            },
+            error: function() {
+                alert("faile to update service config, please try again")
+            }
+        })
+    }
 
     UserMqtt.prototype.restartMqtt = function() {
         $.ajax({
