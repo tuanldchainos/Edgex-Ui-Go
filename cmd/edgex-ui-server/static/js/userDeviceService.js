2a19dc7e1e7e9ab4af05ea1a91638f432980f5f9
tuanldchainos <tuanldhaihaua@gmail.com>
2020-06-16 16:58:15 +0700

    add get button to get config

--- cmd/edgex-ui-server/static/js/userDeviceService.js
+++ cmd/edgex-ui-server/static/js/userDeviceService.js
@@ -11,27 +11,29 @@ orgEdgexFoundry.userZigbeeService = (function(){
     UserZigbeeService.prototype = {
         constructor:UserZigbeeService,
         restartService: null,
+        getConfig: null,
         putConfig: null,
+        renderConfig: null
     }
 
     var userZigbee = new UserZigbeeService()
 
 
     UserZigbeeService.prototype.putConfig = function() {
-        var Network_RequestTimeout = document.getElementById("request-timeout").value
-        var Network_ResponseTimeout = document.getElementById("response-timeout").value
-        var UpdateLastConnected = document.getElementById("update-last-connected").value
+        var NetworkType = document.getElementById("network-type").value
+        var RequestTimeout = document.getElementById("request-timeout").value
+        var CommandResponseTimeout = document.getElementById("command-response-timeout").value
+        var ObjectResponseTimeout = document.getElementById("object-response-timeout").value
         $.ajax({
             url: '/api/v1/user/config/devservice/' + userZigbee.ZigbeeService,
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify({
-                "Device": {
-                    "UpdateLastConnected": UpdateLastConnected
-                },
                 "Driver": {
-                    "Network_RequestTimeout": Network_RequestTimeout,
-                    " Network_ResponseTimeout": Network_ResponseTimeout
+                    "NetworkType": NetworkType,
+                    "NetWorkRequestTimeout": RequestTimeout, 
+                    "NetworkCommandResponseTimeout": CommandResponseTimeout,
+                    "NetworkAddObjectResponseTimeout": ObjectResponseTimeout
                 }
             }),
             success: function(data) {
@@ -41,6 +43,26 @@ orgEdgexFoundry.userZigbeeService = (function(){
                 alert("faile to update service config, please try again")
             }
         })
+    }
+
+    UserZigbeeService.prototype.getConfig = function() {
+        $.ajax({
+            url: '/api/v1/user/config/devservice/' + userZigbee.ZigbeeService,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                let dt = data.Driver
+                userZigbee.renderConfig(dt)
+                alert("Get current device zigbee config successfully")
+            }
+        })
+    }
+
+    UserZigbeeService.prototype.renderConfig = function(dt) {
+        document.getElementById("network-type").value = dt.NetworkType
+        document.getElementById("request-timeout").value = dt.NetWorkRequestTimeout
+        document.getElementById("command-response-timeout").value = dt.NetworkCommandResponseTimeout
+        document.getElementById("object-response-timeout").value = dt.NetworkAddObjectResponseTimeout
     }
 
     UserZigbeeService.prototype.restartService = function() {
